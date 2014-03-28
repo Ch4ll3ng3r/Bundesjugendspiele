877f13b5f109902c6b059efe80729d8c36d4fc56
Bl4ckSc0rp10n <flipp.server@live.de>
2014-03-28 17:31:38 +0100

    Added System Look&Feel

--- Bundesjugendspiele/src/bundesjugendspiele/CBundesjugendspiele.java
+++ Bundesjugendspiele/src/bundesjugendspiele/CBundesjugendspiele.java
@@ -3,10 +3,17 @@ package bundesjugendspiele;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.SpringLayout;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.JTextField;
 
 public class CBundesjugendspiele {
 
 	private JFrame frame;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -16,6 +23,7 @@ public class CBundesjugendspiele {
 			public void run() {
 				try {
 					CBundesjugendspiele window = new CBundesjugendspiele();
+
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -35,9 +43,43 @@ public class CBundesjugendspiele {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame("Bundewsjugendspiele");
+		frame = new JFrame("Bundesjugendspiele");
 		frame.setBounds(100, 100, 800, 400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			
+		
+		SwingUtilities.updateComponentTreeUI(frame);
+		
+		SpringLayout springLayout = new SpringLayout();
+		frame.getContentPane().setLayout(springLayout);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		springLayout.putConstraint(SpringLayout.NORTH, lblNewLabel, 10, SpringLayout.NORTH, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, lblNewLabel, 10, SpringLayout.WEST, frame.getContentPane());
+		frame.getContentPane().add(lblNewLabel);
+		
+		textField = new JTextField();
+		springLayout.putConstraint(SpringLayout.WEST, textField, 6, SpringLayout.EAST, lblNewLabel);
+		springLayout.putConstraint(SpringLayout.SOUTH, textField, 0, SpringLayout.SOUTH, lblNewLabel);
+		springLayout.putConstraint(SpringLayout.EAST, textField, 204, SpringLayout.EAST, lblNewLabel);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
 	}
-
 }
