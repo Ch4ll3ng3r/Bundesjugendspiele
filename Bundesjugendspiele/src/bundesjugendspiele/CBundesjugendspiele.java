c7ed5d79186daa2c70969bf49043bbb9e8c1079e
Bl4ckSc0rp10n <flipp.server@live.de>
2014-03-28 17:05:28 +0100

    CWettkampfkarte approved

--- Bundesjugendspiele/src/bundesjugendspiele/CBundesjugendspiele.java
+++ Bundesjugendspiele/src/bundesjugendspiele/CBundesjugendspiele.java
@@ -3,7 +3,12 @@ package bundesjugendspiele;
 public class CBundesjugendspiele {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		CWettkampfkarte TestSpieler = new CWettkampfkarte("Krenzler", "Simon", "Q1", 1996);
+		System.out.println(TestSpieler.name() + "\n"
+				+ TestSpieler.vorname() + "\n"
+				+ TestSpieler.klasse() + "\n"
+				+ TestSpieler.jahrgang());
 
 	}
 
