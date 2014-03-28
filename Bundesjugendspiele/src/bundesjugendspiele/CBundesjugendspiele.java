3227c8cebf3b00dec652624eabe556860ac354c9
Bl4ckSc0rp10n <flipp.server@live.de>
2014-03-28 17:13:31 +0100

    Start| implementing GUI

--- Bundesjugendspiele/src/bundesjugendspiele/CBundesjugendspiele.java
+++ Bundesjugendspiele/src/bundesjugendspiele/CBundesjugendspiele.java
@@ -1,16 +1,43 @@
 package bundesjugendspiele;
 
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
 public class CBundesjugendspiele {
 
-	public static void main(String[] args) {
-		
-		CWettkampfkarte TestSpieler = new CWettkampfkarte("Krenzler", "Simon", "Q1", 1996);
-		System.out.println(TestSpieler.name() + "\n"
-				+ TestSpieler.vorname() + "\n"
-				+ TestSpieler.klasse() + "\n"
-				+ TestSpieler.jahrgang());
+	private JFrame frame;
 
-		//Window folgt
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CBundesjugendspiele window = new CBundesjugendspiele();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public CBundesjugendspiele() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame("Bundewsjugendspiele");
+		frame.setBounds(100, 100, 800, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
